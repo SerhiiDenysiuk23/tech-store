5063d0f58948b48ea8f815b99fcf3809f7be05c4
Serhii Denysiuk <serhiidenysiuk22@gmail.com>
2024-06-07 23:17:05 +0300

    Changed connection to mongo

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,4 +1,4 @@
-import { connect, connection, ConnectOptions } from 'mongoose';
+import { connect, connection } from 'mongoose';
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
@@ -6,13 +6,9 @@ if (!MONGODB_URI) {
   throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
 }
 
-const options: ConnectOptions = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-};
-
 let mongoosePromise: Promise<typeof import('mongoose')>;
 
+// Расширяем глобальный объект для хранения обещания клиента в режиме разработки
 const globalWithMongoose = global as typeof globalThis & {
   _mongooseClientPromise: Promise<typeof import('mongoose')>
 };
@@ -28,12 +24,12 @@ const connectToDatabase = async () => {
     // В режиме разработки используем глобальную переменную для сохранения обещания
     if (process.env.NODE_ENV === 'development') {
       if (!globalWithMongoose._mongooseClientPromise) {
-        globalWithMongoose._mongooseClientPromise = connect(MONGODB_URI, options);
+        globalWithMongoose._mongooseClientPromise = connect(MONGODB_URI);
       }
       mongoosePromise = globalWithMongoose._mongooseClientPromise;
     } else {
       // В режиме продакшн создаем новое подключение при каждом вызове
-      mongoosePromise = connect(MONGODB_URI, options);
+      mongoosePromise = connect(MONGODB_URI);
     }
 
     await mongoosePromise;
