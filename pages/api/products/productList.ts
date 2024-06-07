c1d087c076985366b0c8c00f41980c56c9446c7d
Serhii Denysiuk <serhiidenysiuk22@gmail.com>
2024-06-07 22:16:57 +0300

    Added products to main page

--- pages/api/products/productList.ts
+++ pages/api/products/productList.ts
@@ -1,9 +1,8 @@
 import Product from "@/models/Product";
-import User from "@/models/User";
-import jwt from "jsonwebtoken";
 import {NextApiRequest, NextApiResponse} from "next";
 import connectToDatabase from "@/lib/mongodb";
 import Category from "@/models/Category";
+import Brand from "@/models/Brand";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   await connectToDatabase();
@@ -13,6 +12,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
       const products = await Product.find({}).populate("category").populate("brand");
       return res.status(200).json({ products: products });
     } catch (error) {
+      console.log(error)
       return res.status(500).json({ message: 'Internal Server Error' });
     }
   } else {
